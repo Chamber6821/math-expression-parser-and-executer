ee7a3d2a9d01760608163c793d44340ce6ef7379
Chamber6821 <vania.osinin@yandex.ru>
2022-10-23 23:45:39 +0300

    fix: no skip if we get not expected token

--- src/parsing.c
+++ src/parsing.c
@@ -11,25 +11,27 @@ source_t *skipSpaces(source_t *source) {
 token_t parseToken(source_t *source) {
     skipSpaces(source);
 
-    while (!isEnd(source)) {
-        char ch = get(source);
+    if (isEnd(source)) return onlyType(END);
 
-        source->marker++;
-        switch (ch) {
-            case '(': return onlyType(OPEN_PAR);
-            case ')': return onlyType(CLOSE_PAR);
-            case '+': return onlyType(BIN_ADD);
-            case '*': return onlyType(BIN_MULTIPLY);
-            default: break;
-        }
-        source->marker--;
+    char ch = get(source);
 
-        int value, read = 0;
-        int scanned = sscanf_s(source->marker, "%i%n", &value, &read);
-        if (scanned >= 1) {
-            source->marker += read;
-            return number(value);
+    source->marker++;
+    switch (ch) {
+        case '(': return onlyType(OPEN_PAR);
+        case ')': return onlyType(CLOSE_PAR);
+        case '+': return onlyType(BIN_ADD);
+        case '*': return onlyType(BIN_MULTIPLY);
+        default: {
+            source->marker--;
+            break;
         }
+    }
+
+    int value, read = 0;
+    int scanned = sscanf_s(source->marker, "%i%n", &value, &read);
+    if (scanned >= 1) {
+        source->marker += read;
+        return number(value);
     }
 
     return onlyType(END);
